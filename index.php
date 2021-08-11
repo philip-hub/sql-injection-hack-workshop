8e05cf55f39cb5c7af6f02c871de10d4a23aac0b
Philip Pounds <philipstudentemail@gmail.com>
2021-08-11 00:08:24 -0500

    I fixed this to display bank info

--- index.php
+++ index.php
@@ -10,8 +10,25 @@ if ($conn->connect_error) {
 // Make a SQL query
 $username=$_POST["username"];
 $password = $_POST["password"];
-$sql = "SELECT * FROM Users WHERE Name ='".$username."' AND Pass ='".$password."'";
+$sql = "SELECT * FROM userinfo WHERE username ='".$username."' AND password ='".$password."';";
+echo $sql;
 $result = mysqli_query($conn, $sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    $row_check ="";
+    while($row = $result->fetch_assoc()) {
+        
+        
+            $bank_info = "<br> <p>Your username in case you forgot ". $row["username"]. " - Your password : ". $row["password"]. "  Your hard earned money" . $row["amount"] . "</p><br>";
+           
+        
+    }
+} else {
+    $bank_info = "<p>Sorry 0 results</p>";
+}
+
+
 ?>
 <!DOCTYPE html>
 <html>
